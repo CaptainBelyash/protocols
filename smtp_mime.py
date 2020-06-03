a3666be0a2dfb4dcf0cf1fc82db05b68a7dce09e
CaptainBelyash <50026308+CaptainBelyash@users.noreply.github.com>
2020-06-03 23:03:46 +0500

    smpt_mime done

--- smtp_mime.py
+++ smtp_mime.py
@@ -1,11 +1,19 @@
 
+import argparse
+import os
+import sys
+import glob
 import socket
 import ssl
 import base64
+from getpass import getpass
 
 
-host_name = 'smtp.gmail.com'
+image_exts = ['gif', 'png', 'jpeg', 'jpg']
+image_files = []
+
 context = ssl.create_default_context()
+verbosel = False
 
 
 class ServerAnswer:
@@ -24,20 +32,22 @@ def get_answer(sock):
 
 
 def print_answers(answers, inbase64=False):
-    throw = False
     for answer in answers:
         if answer.code[0] in {'4', '5'}:
-            throw = True
+            print(answer.code, answer.msg, file=sys.stderr)
+            raise Exception
         if inbase64:
-            print(answer.code,
-                  base64
-                  .b64decode(answer.msg)
-                  .decode('utf-8'))
+            if verbosel:
+                print(answer.code,
+                      base64
+                      .b64decode(answer.msg)
+                      .decode('utf-8'))
         else:
-            print(answer.code, answer.msg)
-    print()
-    if throw:
-        raise Exception
+            if verbosel:
+                print(answer.code, answer.msg)
+
+    if verbosel:
+        print()
 
 
 def send_msg(sock, msg):
@@ -46,8 +56,75 @@ def send_msg(sock, msg):
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ssl', default=False, type=bool,
+                        help='разрешить использование ssl, если сервер '
+                             'поддерживает (по умолчанию не использовать)')
+    parser.add_argument('-s', '--serve',
+                        help='адрес (или доменное имя) SMTP-сервера в формате '
+                             'адрес[:порт] (порт по умолчанию 25)',
+                        required=True)
+    parser.add_argument('-t', '--to',
+                        help='почтовый адрес получателя письма',
+                        required=True)
+    parser.add_argument('-f', '--from', default='',
+                        help='почтовый адрес отправителя'
+                             '(по умолчанию <>)')
+    parser.add_argument('--subject', default='Happy Pictures',
+                        help='необязательный параметр, задающий тему письма'
+                             '(по умолчанию "Happy Pictures")')
+    parser.add_argument('-auth', default=False, type=bool,
+                        help='Запрос авторизации (по умолчанию нет)')
+    parser.add_argument('-v', '--verbosel', default=False, type=bool,
+                        help='Отображение протокола работы (по умолчанию нет)')
+    parser.add_argument('-d', '--directory', default='',
+                        help='Каталог с изображениями (по умолчанию $pwd)')
+
+    args = parser.parse_args().__dict__
+    verbosel = args['verbosel']
+    message = make_message(args['from'], args['to'], args['subject'], args['directory'])
+    smtp_client(message, args['serve'], args['from'],
+                args['to'], args['ssl'], args['auth'])
+
+
+def make_message(msg_from, msg_to, subject, directory):
+    message = f'''From: <{msg_from}>
+To: <{msg_to}>
+Subject: {subject}
+Content-Type: multipart/mixed; boundary=bound
+
+'''
+    for ext in image_exts:
+        image_files.extend(glob.glob(f'{directory}*.{ext}'))
+    for image_file in image_files:
+        filename, fileext = os.path.splitext(image_file)
+        fileext = fileext[1:]
+        filename = os.path.basename(filename)
+        with open(image_file, "rb") as file:
+            encoded_string = base64.b64encode(file.read()).decode('utf-8')
+            message += f'''--bound
+Content-Type: image/{fileext}
+Content-Transfer-Encoding: base64
+Content-disposition:attachment; filename="{filename}.{fileext}"
+
+{encoded_string}
+
+'''
+    message += '''--bound--
+
+\r\n.\r\n'''
+    return message
+
+
+def smtp_client(message, smtp_server, msg_from, msg_to, use_ssl, use_auth):
+    old_sock = None
+    host_name = smtp_server
+    host_port = 25
+    if ':' in smtp_server:
+        host_name, host_port = host_name.split(':')
+        host_port = int(host_port)
     sock = socket.socket()
-    sock.connect((host_name, 587))
+    sock.connect((host_name, host_port))
 
     print_answers(get_answer(sock))
 
@@ -57,62 +134,41 @@ def main():
     send_msg(sock, 'STARTTLS')
     print_answers(get_answer(sock))
 
-    with context.wrap_socket(sock,
-                             server_hostname=host_name) as sslsock:
-        send_msg(sslsock, 'EHLO x')
-        print_answers(get_answer(sslsock))
+    if use_ssl:
+        old_sock = sock
+        sock = context.wrap_socket(sock,
+                             server_hostname=host_name)
+    send_msg(sock, 'EHLO x')
+    print_answers(get_answer(sock))
+    if use_auth:
+        send_msg(sock, 'AUTH LOGIN')
 
-        send_msg(sslsock, 'AUTH LOGIN')
-
-        print_answers(get_answer(sslsock), True)
+        print_answers(get_answer(sock), True)
+        print('Login:', end=' ')
         username = base64.b64encode(input()
                                     .encode('utf-8')).decode('utf-8')
-        send_msg(sslsock, username)
+        send_msg(sock, username)
 
-        print_answers(get_answer(sslsock), True)
-        password = base64.b64encode(input()
-                                    .encode('utf-8')).decode('utf-8')
-        send_msg(sslsock, password)
+        print_answers(get_answer(sock), True)
+        password = getpass()
+        password = base64.b64encode(password
+                                    .encode('utf-8'))\
+            .decode('utf-8')
+        send_msg(sock, password)
+        print_answers(get_answer(sock))
 
-        print_answers(get_answer(sslsock))
+    send_msg(sock, f'mail from: <{msg_from}>')
+    print_answers(get_answer(sock))
+    send_msg(sock, f'rcpt to: <{msg_to}>')
+    print_answers(get_answer(sock))
 
-        send_msg(sslsock, f'mail from: <{username}>')
-        print_answers(get_answer(sslsock))
-        next = input()
-        while next != '.':
-            send_msg(sslsock, f'rcpt to: <{next}>')
-            print_answers(get_answer(sslsock))
-            next = input()
-        with open('image.jpg', 'rb') as image:
-            encoded_string = base64.b64encode(image.read()).decode('utf-8')
-        send_msg(sslsock, 'data')
-        print_answers(get_answer(sslsock))
-        send_msg(sslsock,
-                 f'''From: BIBANIUM <e@mail>
-To: BOBINIUM <e@mail>
-Subject: BIBA
-Date: Fri, 26 Mar 2020 09:00:00 GMT+5
-Content-Type: multipart/related; boundary=bound
-
-
---bound
-Content-Type: text/html; charset=utf-8
-
-<img src="cid:10"><br> 
-
---bound
-Content-Type: image/jpg
-Content-Transfer-Encoding: base64
-Content-disposition:attachment; filename="python.jpg"
-Content-Id: 10
-
-{encoded_string}
-
---bound--
-
-\r\n.\r\n''')
-        print_answers(get_answer(sslsock))
-        send_msg(sslsock, 'QUIT')
+    send_msg(sock, 'data')
+    print_answers(get_answer(sock))
+    send_msg(sock, message)
+    print_answers(get_answer(sock))
+    send_msg(sock, 'QUIT')
+    if old_sock:
+        old_sock.close()
     sock.close()
 
 
